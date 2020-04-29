4589b466a5ca5321a668b9338a43043f948e35e4
cjwright96 <christopher.wright@bristol.ac.uk>
2020-04-29 10:47:41 +0100

    implimenting the testing functions

--- tests/test_gini.py
+++ tests/test_gini.py
@@ -20,6 +20,8 @@ def test_gini_single():
     test calculating a gini coefficient with a single item in the list
     the coefficient should be zero as there's no variation
     '''
+    gini = calculate_gini(pd.Series([1.0]))
+    
     raise NotImplementedError("Please Implement This Test")
 
 def test_gini_four():
